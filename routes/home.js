ac993ceda67551cadab47160c24179b87d2fb81b
Joseph Effiom Edet <90644500+AwesomeEJE@users.noreply.github.com>
2023-04-18 17:13:15 +0100

    MVP

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.status(200).json("api");
+  res.status(200).json("api...");
 });
 
 export default router;
